9381551e7fea7267a813266b9beb832ec4cf4ea0
Jeff Taggart <jeff@jetaggart.com>
2014-08-14 08:05:07 -0600

    Move script to application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,36 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+//
+
+$(document).on('ready', function() {
+  var map;
+  AmCharts.ready(function() {
+    map                  = new AmCharts.AmMap();
+    map.pathToImages     = "http://www.amcharts.com/lib/3/images/";
+    map.panEventsEnabled = true;
+    map.backgroundColor  = "#535364";
+    map.backgroundAlpha  = 1;
+    map.zoomControl.panControlEnabled  = false;
+    map.zoomControl.zoomControlEnabled = false;
+
+    var dataProvider     = {
+      mapVar: AmCharts.maps.usaHigh,
+      getAreasFromMap: true,
+      areas: []
+    };
+
+    map.dataProvider     = dataProvider;
+    map.areasSettings    = {
+      autoZoom: true,
+      color: "#CDCDCD",
+      colorSolid: "#5EB7DE",
+      selectedColor: "#5EB7DE",
+      outlineColor: "#666666",
+      rollOverColor: "#88CAE7",
+      rollOverOutlineColor: "#FFFFFF"
+    };
+
+    map.write("mapdiv");
+  });
+})
